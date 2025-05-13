b9235e93af1860e9f5aade5a522ff33e09cd26b2
Matias Daniel Alessandrello <matialess@gmail.com>
2025-05-12 23:53:09 -0300

    agregamos nuestra aplicacion a los settings

--- crud/models.py
+++ crud/models.py
@@ -3,3 +3,11 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    is_created = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created']
+        
+    def __str__(self):
+        return self.title
